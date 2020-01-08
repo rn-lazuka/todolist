6f91cd0fe4e3e9a1d7837675c457dfc6f1c10776
Roman Lazuka <rn.lazuka@gmail.com>
2020-01-08 23:24:17 +0300

    added saving to local storage

--- src/App.js
+++ src/App.js
@@ -6,50 +6,94 @@ import TodoListFooter from "./TodoListFooter";
 
 
 class App extends React.Component {
+    nextTaskId = 0;
     state = {
         filterValue: "All",
-        tasks: [
-            {title: 'JS', isDone: true, priority: 'low'},
-            {title: 'HTML', isDone: true, priority: 'medium'},
-            {title: 'CSS', isDone: true, priority: 'high'},
-            {title: 'React', isDone: false, priority: 'low'}
-        ]
+        tasks: []
+    };
+
+
+    componentDidMount() {
+        this.restoreState();
+
+    }
+
+    saveState = () => {
+        let stateAsString = JSON.stringify(this.state);
+        localStorage.setItem("our-state", stateAsString);
+
+    };
+
+    restoreState = () => {
+        let state = {
+            filterValue: "All",
+            tasks: []
+        };
+        let stateAsString = localStorage.getItem("our-state");
+        if (stateAsString != null) {
+            state = JSON.parse(stateAsString);
+        }
+        this.setState(state, () => {
+            // this.nextTaskId = this.state.tasks.reduce((task, curr) => task.id > curr.id ? task.id : curr.id, 0) + 1;
+            this.state.tasks.filter (task =>{if(task.id>=this.nextTaskId){
+                this.nextTaskId=task.id+1
+            }})
+        });
+
     };
 
     changeFilter = (newFilterValue) => {
         this.setState({
             filterValue: newFilterValue
+        }, () => {
+            this.saveState()
         });
     };
     addTask = (newText) => {
         if (newText !== "") {
 
             let newTask = {
+                id: this.nextTaskId,
                 title: newText,
                 isDone: false,
                 priority: 'low'
             };
+            this.nextTaskId++;
             let newTasks = [...this.state.tasks, newTask];
-            this.setState({tasks: newTasks});
+            this.setState({tasks: newTasks}, () => {
+                this.saveState();
+            });
         }
     };
 
-    changeStatus = (status, task) => {
-        let tasksCopy = this.state.tasks.map(t => {
-            if (t === task) {
-                return {...t, isDone: status}
+
+    changeTask = (taskId, obj) => {
+        let newTasks = this.state.tasks.map(t => {
+            if (t.id != taskId) {
+                return t
+            } else {
+                return {...t, ...obj}
             }
-            return t
         });
-        this.setState({tasks: tasksCopy});
+        this.setState({tasks: newTasks});
     };
+
+    changeStatus = (isDone, taskId) => {
+        this.changeTask(taskId, {isDone: isDone})
+    };
+
+    changeTitle = (title, taskId) => {
+        this.changeTask(taskId, {title: title})
+    };
+
     render = () => {
 
         return (
             <div className="App">
                 <div className="todoList">
                     <TodoListHeader addTask={this.addTask}/>
-                    <TodoListTasks changeStatus={this.changeStatus}
+                    <TodoListTasks changeTitle={this.changeTitle}
+                                   changeStatus={this.changeStatus}
                                    tasks={this.state.tasks.filter((t) => {
                                        switch (this.state.filterValue) {
                                            case "All":
--- src/TodoListTask.js
+++ src/TodoListTask.js
@@ -2,15 +2,30 @@ import React from 'react';
 import './App.css';
 
 class TodoListTask extends React.Component {
+    state ={editMode:false,
+    };
+    activateEditMode =()=>{
+     this.setState({editMode:true})
+    };
+    deactivateEditMode =()=>{
+     this.setState({editMode:false})
+    };
+
+    onTitleChanged =(e)=>{
+       this.props.changeTitle(e.currentTarget.value,this.props.task.id)
+    };
+
     onIsDoneChanged = (e) => {
-        this.props.changeStatus(e.currentTarget.checked, this.props.task)
+        this.props.changeStatus(e.currentTarget.checked, this.props.task.id)
     };
     render = () => {
         let classForTask = this.props.task.isDone?"todoList-task done":"todoList-task";
         return (
             <div className={classForTask}>
                 <input type="checkbox" checked={this.props.task.isDone} onChange={this.onIsDoneChanged}/>
-                <span>{this.props.task.title}</span>
+                {this.state.editMode
+                ?<input onChange={this.onTitleChanged} onBlur={this.deactivateEditMode} autoFocus={true} value={this.props.task.title}/>
+                :<span onClick={this.activateEditMode}>{this.props.task.id} -{this.props.task.title}</span>}
                 <span>, priority: {this.props.task.priority}</span>
             </div>
         );
